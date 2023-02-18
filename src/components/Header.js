8731b2e335579e7453d2c06f1555321f64595d3d
VuHuong511 <huongvtgcd191185@fpt.edu.vn>
2023-02-18 12:36:57 +0700

    header dynamic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,28 @@
-import React from "react";
-
-// import link
-import { Link } from "react-router-dom";
-
-// import logo
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Logo from "../assets/img/logo.png";
 
-const Header = () => {
+export default function Header() {
+  const [pageState, setPageState] = useState("login")
+  const location = useLocation();
+  const navigate = useNavigate();
+  const auth = getAuth();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) =>{
+      if(user){
+        setPageState("profile");
+      }
+      else {
+        setPageState("login");
+      }
+    });
+  }, [auth]);
+  function matchRoute(route) {
+    if (route === location.pathname){
+      return true;
+    }
+  }
   return (
     <header>
       <div className="container mx-auto flex justify-between items-center">
@@ -16,22 +32,29 @@ const Header = () => {
         </Link>
         {}
         <div className="flex items-center gap-6">
-          <Link className="hover:text-blue-900 transition" to="">
-            Home
-          </Link>
+        <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                matchRoute("/") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/")}
+            >
+              Home
+            </li>
           <Link className="hover:text-blue-900 transition" to="/about">
             About
           </Link>
-          <Link
-            className="bg-blue-700 hover:bg-blue-800 text-white px-4 py-3 rounded-lg transition"
-            to="/login"
-          >
-            Log in
-          </Link>
+          <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                (matchRoute("/sign-in") || matchRoute("/profile")) &&
+                "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/profile")}
+            >
+              {pageState}
+            </li>
+      
         </div>
       </div>
     </header>
   );
-};
-
-export default Header;
+}
