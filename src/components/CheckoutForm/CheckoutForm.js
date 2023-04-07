652db5fb82749cb807851da3827efb202bfdd912
VuHuong <vuhuong51101@gmail.com>
2023-04-08 04:52:55 +0700

    check out form component

--- src/components/CheckoutForm/CheckoutForm.js
+++ src/components/CheckoutForm/CheckoutForm.js
@@ -1,7 +1,106 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  PaymentElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+import "./CheckoutForm.css";
+import DepositSummary from "../DepositSummary/DepositSummary";
 
 const CheckoutForm = () => {
-  return <div>CheckoutForm</div>;
-};
+  const stripe = useStripe();
+  const elements = useElements();
 
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (!stripe) {
+      return;
+    }
+
+    const clientSecret = new URLSearchParams(window.location.search).get(
+      "payment_intent_client_secret"
+    );
+
+    if (!clientSecret) {
+      return;
+    }
+  }, [stripe]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js has not yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    setIsLoading(true);
+
+    const { error } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        // Make sure to change this to your payment completion page
+        return_url: "http://localhost:3000",
+      },
+    });
+
+    // This point will only be reached if there is an immediate error when
+    // confirming the payment. Otherwise, your customer will be redirected to
+    // your `return_url`. For some payment methods like iDEAL, your customer will
+    // be redirected to an intermediate site first to authorize the payment, then
+    // redirected to the `return_url`.
+    if (error.type === "card_error" || error.type === "validation_error") {
+      setMessage(error.message);
+    } else {
+      setMessage("An unexpected error occurred.");
+    }
+
+    setIsLoading(false);
+  };
+
+  const paymentElementOptions = {
+    layout: "tabs",
+  };
+
+  return (
+    <section>
+      <div className="checkout">
+        <h2>Checkout</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <card className="card">
+              <DepositSummary />
+            </card>
+          </div>
+          <div>
+            <card className="pay">
+              <h3>Stripe checkout</h3>
+
+              <PaymentElement options={paymentElementOptions} />
+              <button
+                disabled={isLoading || !stripe || !elements}
+                id="submit"
+                className="button"
+              >
+                <span id="button-text">
+                  {isLoading ? (
+                    <div className="spinner" id="spinner"></div>
+                  ) : (
+                    "Pay now"
+                  )}
+                </span>
+              </button>
+              {/* Show any error or success messages */}
+              {message && <div id="payment-message">{message}</div>}
+            </card>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
 export default CheckoutForm;
