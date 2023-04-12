918cf6f5f8bb8116b12de9742f83878d3151eb55
VuHuong <vuhuong51101@gmail.com>
2023-04-12 21:37:34 +0700

    information box logic

--- src/components/Admin/DashBoard/Dashboard.js
+++ src/components/Admin/DashBoard/Dashboard.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Dashboard.css";
 import InfoBox from "../../InforBox/InfoBox";
 import { AiFillDollarCircle } from "react-icons/ai";
 import { BsCart4 } from "react-icons/bs";
 import { FaCartArrowDown } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { STORE_ROOMS, selectRooms } from "../../../redux/slice/roomSlice";
+import {
+  CALCULATOR_TOTAL_RESERVATION,
+  STORE_RESERVATION,
+  selectReservationAmount,
+  selectReservationHistory,
+} from "../../../redux/slice/reservationSlice";
+import useEffectCollection from "../../../hooks/useFetchCollection";
 const earningIcon = <AiFillDollarCircle size={30} color="#b624ff" />;
 const productIcon = <BsCart4 size={30} color="#1f93ff" />;
 const ordersIcon = <FaCartArrowDown size={30} color="orangered" />;
 const Dashboard = () => {
+  const rooms = useSelector(selectRooms);
+  const reservations = useSelector(selectReservationHistory);
+  const totalReservationAmount = useSelector(selectReservationAmount);
+
+  const fbRooms = useEffectCollection("listings");
+  const { data } = useEffectCollection("reservation");
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(
+      STORE_ROOMS({
+        rooms: fbRooms.data,
+      })
+    );
+
+    dispatch(STORE_RESERVATION(data));
+
+    dispatch(CALCULATOR_TOTAL_RESERVATION());
+  }, [dispatch, data, fbRooms]);
   return (
     <div>
       <h2>DashBoard</h2>
@@ -15,19 +43,19 @@ const Dashboard = () => {
       <InfoBox
         className="card"
         title={"Earnings"}
-        count={188}
+        count={`$${totalReservationAmount}`}
         icon={earningIcon}
       />
       <InfoBox
         className="card"
         title={"Rooms"}
-        count={188}
+        count={rooms.length}
         icon={productIcon}
       />
       <InfoBox
         className="card"
         title={"Reservation"}
-        count={188}
+        count={reservations.length}
         icon={ordersIcon}
       />
     </div>
--- src/redux/slice/reservationSlice.js
+++ src/redux/slice/reservationSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   reservationHistory: [],
+  totalReservationAmount: null,
 };
 
 const reservationSlice = createSlice({
@@ -11,10 +12,24 @@ const reservationSlice = createSlice({
     STORE_RESERVATION(state, action) {
       state.reservationHistory = action.payload;
     },
+    CALCULATOR_TOTAL_RESERVATION(state, action) {
+      const array = [];
+      state.reservationHistory.map((item) => {
+        const { reservationAmount } = item;
+        return array.push(reservationAmount);
+      });
+      const totalAmount = array.reduce((a, b) => {
+        return a + b;
+      }, 0);
+      state.totalReservationAmount = totalAmount;
+    },
   },
 });
 
-export const { STORE_RESERVATION } = reservationSlice.actions;
+export const { STORE_RESERVATION, CALCULATOR_TOTAL_RESERVATION } =
+  reservationSlice.actions;
 export const selectReservationHistory = (state) =>
   state.reservation.reservationHistory;
+export const selectReservationAmount = (state) =>
+  state.reservation.totalReservationAmount;
 export default reservationSlice.reducer;
